3421b5409b6ee4a665b34ed07514d2bed0339aac
franknhat <franknhat@yahoo.com>
2022-08-11 21:22:58 -0500

    served the App directory as the viewRoot

--- App/router.js
+++ App/router.js
@@ -7,23 +7,23 @@ const router = new Router();
 //making the router use the view engine to allow render of .ejs files
 router.use(
     viewEngine(oakAdapter, dejsEngine, {
-        viewRoot: `${Deno.cwd()}/App/static`,
+        viewRoot: `${Deno.cwd()}/App`,
     }),
 );
 
 
 router
     .get("/", async (ctx) => {
-        ctx.render("Templates/index.ejs"); //issue with background img
+        ctx.render("static/Templates/index.ejs"); //issue with background img
     })
     .get("/projects", async (ctx) => {
-        ctx.render("Templates/projects.ejs", {});
+        ctx.render("static/Templates/projects.ejs", {});
     })
     .get("/contact-us", async (ctx, next) => {
-        ctx.render("Templates/contact-us.ejs", {});
+        ctx.render("static/Templates/contact-us.ejs", {});
     })
     .get("/services", async (ctx, next) => {
-        ctx.render("Templates/services.ejs", {});
+        ctx.render("static/Templates/services.ejs", {});
     });
     
 export default router;
